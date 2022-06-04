01e0e4eedad4a91c896d439b1a82f405e6a63936
PatWeid <patrick.weidmueller@gmail.com>
2022-06-04 23:39:26 +0200

    Add validation

--- src/views/AddStudent.vue
+++ src/views/AddStudent.vue
@@ -7,58 +7,58 @@ import Datepicker from 'vue3-datepicker'
   <h1>Add Student</h1>
 
 
-  <!--  <v-card title="Add Student">-->
+  <v-card title="Add Student">
 
-  <!--    <v-form ref="form" v-model="valid" lazy-validation>-->
-  <v-container>
-    <v-row justify="space-around">
-      <v-col>
-        <v-text-field v-model="id" :rules="idRules" label="Student ID"></v-text-field>
-      </v-col>
-      <v-col>
-        <v-text-field v-model="firstName" label="Firstname"></v-text-field>
-      </v-col>
-      <v-col>
-        <v-text-field v-model="lastName" label="Lastname"></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row justify="center">
-      <v-col><p>DOB:</p></v-col>
-      <v-col>
-        <datepicker @click="validateDOB" selected="" v-model="dob">DOB</datepicker>
-      </v-col>
-      <v-col><p>Joining Date:</p></v-col>
-      <v-col>
-        <datepicker @click="validateDOB" selected="" v-model="joiningDate">DOB</datepicker>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-container class="px-0" fluid>
-        <v-radio-group @change="addStudent" v-model="radioGroup">
-          <v-radio label="male" value="male"></v-radio>
-          <v-radio label="female" value="female"></v-radio>
-        </v-radio-group>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-container>
+        <v-row justify="space-around">
+          <v-col>
+            <v-text-field v-model="id" :rules="idRules" label="Student ID"></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field v-model="firstName" :rules="firstNameRules" label="Firstname" ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field v-model="lastName" label="Lastname"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-col><p>DOB:</p></v-col>
+          <v-col>
+            <datepicker @click="validateDOB" selected="" v-model="dob">DOB</datepicker>
+          </v-col>
+          <v-col><p>Joining Date:</p></v-col>
+          <v-col>
+            <datepicker @click="validateDOB" selected="" v-model="joiningDate" :upper-limit="today">DOB</datepicker>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-container class="px-0" fluid>
+            <v-radio-group @change="addStudent" v-model="radioGroup">
+              <v-radio label="male" value="male"></v-radio>
+              <v-radio label="female" value="female"></v-radio>
+            </v-radio-group>
+          </v-container>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-select :rules="departmentRules"
+                :items="departments"
+                label="Department"
+                v-model="department"
+            ></v-select>
+          </v-col>
+          <v-col>
+            <v-text-field v-model="email" :rules="emailRules" label="E-Mail"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-btn :disabled="!valid" @click="addStudent">Add Student</v-btn>
       </v-container>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-select
-            :items="departments"
-            label="Department"
-            v-model="department"
-        ></v-select>
-      </v-col>
-      <v-col>
-        <v-text-field v-model="email" label="E-Mail"></v-text-field>
-      </v-col>
-    </v-row>
-    <v-btn :disabled="!valid" @click="addStudent">Add Student</v-btn>
-  </v-container>
 
 
-  <!--    </v-form>-->
+    </v-form>
 
-  <!--  </v-card>-->
+  </v-card>
 
 
 </template>
@@ -70,11 +70,24 @@ export default {
     id: '',
     dob: new Date(),
     joiningDate: new Date(),
+    today: new Date(),
     firstName: '',
     lastName: '',
     email: '',
     valid: true,
-    idRules: [],
+    idRules: [
+      v => !!v || 'Student ID is required',
+      v => Number.isInteger(Number(v)) || 'Not a Number'],
+    firstNameRules: [
+      v => !!v || 'Firstname is required',
+    ],
+    emailRules: [
+      v => !!v || 'Email is required',
+      v => !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+    ],
+    departmentRules: [
+        v => v != 0 || 'Select Department'
+    ],
     radioGroup: 0,
     departments: ['Computer Science', 'Mathematics', 'Engineering'],
     department: '',
